0c1c38daf327b2737a7a9127aaeb5dc3ffeae06f
sineco <sineco@gmail.com>
2015-08-10 20:39:36 -0400

    Added the car detection files

---
+++ build/CMakeFiles/demoCarDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/demoCarDetection.dir/demoCarDetection.cpp.o"
+  "demoCarDetection.pdb"
+  "demoCarDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/demoCarDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
